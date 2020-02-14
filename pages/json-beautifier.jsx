ebaa3311971378067b39009acb0abc3d622761f8
So Vengleab <sovengleab@gmail.com>
2020-02-14 23:34:00 +0700

    [PAGE] WIP Add json beautifier

--- pages/json-beautifier.jsx
+++ pages/json-beautifier.jsx
@@ -1,12 +1,35 @@
-import React from "react";
 import Layout from "../components/Layout";
 import PageContext from "../contexts/page";
 import { PAGE } from "../constants/PageURL";
+import { Segment, Grid, Form, Button, Divider, Icon, TextArea } from "semantic-ui-react";
 
 export default function JSON_Beautifier() {
   return (
     <PageContext.Provider value={{ activeItem: PAGE.JSON_BEAUTIFIER }}>
-      <Layout title="Vengleab Utilities" />
+      <Layout title="Vengleab Utilities">
+        <Segment>
+          <Grid columns={2} relaxed="very" stackable>
+            <Grid.Column>
+              <Form>
+                <TextArea placeholder='Tell us more' style={{ minHeight: 500 }} />
+              </Form>
+            </Grid.Column>
+
+            <Grid.Column verticalAlign="middle" stackable>
+            <Form>
+              <TextArea placeholder='Tell us more' style={{ minHeight: 500 }} />
+            </Form>
+            </Grid.Column>
+          </Grid>
+
+          <Divider vertical>
+            <Button><Icon name="long arrow alternate right" /></Button>
+            <br/>
+            <br/>
+            <Button><Icon name="long arrow alternate left" /></Button>
+          </Divider>
+        </Segment>
+      </Layout>
     </PageContext.Provider>
   );
 }
