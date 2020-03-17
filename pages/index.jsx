b312ebd27ecfadfbbec959da9cf8bd0211597259
so vengleab <vengleab.so@axa.com.sg>
2020-03-17 17:20:15 +0700

    [PAGE] Set index page as string length page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,8 @@
 import React from 'react';
-import Layout from '../components/Layout';
-import PageContext from '../contexts/page';
-import { PAGE } from '../constants/PageURL';
+import StringLength from './string-len';
 
 export default function Index() {
   return (
-    <PageContext.Provider value={{ activeItem: PAGE.INDEX }}>
-      <Layout title="Vengleab Utilities" />
-    </PageContext.Provider>
+    <StringLength/>
   );
 }
