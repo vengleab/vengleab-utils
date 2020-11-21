1134e9390c55dddd941298ce1ab863a1dedcd484
So Vengleab <sovengleab@gmail.com>
2020-11-21 22:37:41 +0700

    [CONFIG] cors

--- server/middleware/dbMiddleware.js
+++ server/middleware/dbMiddleware.js
@@ -1,4 +1,10 @@
 import nextConnect from 'next-connect';
+import Cors from 'cors';
+
+const cors = Cors({
+  origin: '*',
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+});
 
 const mongoose = require('mongoose');
 const env = require('../../constants/env');
@@ -11,8 +17,22 @@ const connection = mongoose.connect(env.MONG_URL, {
 });
 console.log('Connected to DB');
 
+function runMiddleware(req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
 async function database(req, res, next) {
+  runMiddleware(req, res, cors);
   req.connection = connection;
+  await cors(req, res);
   return next();
 }
 
